86ea85157f97886093abfe4fec3b00743c453afd
tonyloba <tonyloba@gmail.com>
2021-10-13 00:26:47 +0000

    added 4_queries

---
+++ 4_queries/9_cohortAvgAssistDur.sql
@@ -0,0 +1,9 @@
+SELECT avg(total_duration) as average_total_duration 
+FROM (
+ SELECT cohorts.name as cohort, sum(completed_at - started_at) as total_duration
+FROM cohorts
+JOIN students ON cohorts.id = cohort_id
+JOIN assistance_requests ON students.id = student_id
+GROUP BY cohort
+ORDER BY total_duration
+) AS table_total_durations;
