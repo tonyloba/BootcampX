4c057f4be9b2d6e47ac1d2b8392f2cb4cfaf0dfa
tonyloba <tonyloba@gmail.com>
2021-10-15 03:40:28 +0000

    fixed error

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -24,7 +24,7 @@ pool.query(sqlQuery,[`%${cohortInput}%`,cohortLimit])
 .then(res => {
   // console.log(res.rows);
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id,} and was in the ${user.cohort} cohort`);
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
 })
 })
 .catch(err => console.error('query error', err.stack));
