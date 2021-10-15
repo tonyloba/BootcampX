716fce48aeeeef3f0a7df3c7142b760c7dc6b8bc
tonyloba <tonyloba@gmail.com>
2021-10-15 03:39:34 +0000

    updated students file

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -26,5 +26,6 @@ pool.query(sqlQuery,[`%${cohortInput}%`,cohortLimit])
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id,} and was in the ${user.cohort} cohort`);
 })
+})
 .catch(err => console.error('query error', err.stack));
 
